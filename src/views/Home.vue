838625baabc67ab381f04896b094c9833020ceec
Muhammad Essa <iMuhammadessa@gmail.com>
2025-05-28 16:58:46 +0500

    icons and responsive

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
-        <h1>Home page is Here</h1>
+    <div class="flex justify-center items-center h-53">
+        <h1 class="text-3xl bg-clip-text text-transparent bg-gradient-to-r from-sky-600 to-pink-500 uppercase">Home page is Here</h1>
     </div>
 </template>
